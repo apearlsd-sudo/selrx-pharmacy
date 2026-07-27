4993d84760dc49ba2937bd14333a8ed88db64b74
Z User <z@container>
2026-07-27 15:58:04 +0000

    ed553950-eaeb-4067-9a80-3faf7688f68e

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 cd /home/z/my-project
-while true; do
-  PORT=3000 HOSTNAME=0.0.0.0 node .next/standalone/server.js >> /tmp/gazpharm-server.log 2>&1
-  echo "Server exited with code $?. Restarting in 1s..." >> /tmp/gazpharm-server.log
-  sleep 1
-done
+PORT=3000 HOSTNAME=0.0.0.0 node node_modules/next/dist/bin/next start --port 3000 -H 0.0.0.0
